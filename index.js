00fd2a083ecfc0544ffc5be2fb4d1592d18f91ef
Beata Kosmider <kosmibe@gmail.com>
2023-02-03 11:47:38 +0100

    task is done

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fillElement = (text) => {
+  const createSpan = document.createElement("span");
+  createSpan.innerText = text;
+  return createSpan;
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
 
